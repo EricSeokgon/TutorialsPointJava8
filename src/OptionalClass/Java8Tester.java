61ef7460bd944273a17f1efe03c0bd365b1909cf
hadesleee <leesk55@gmail.com>
2015-08-05 09:27:44 +0900

    Optional Example

---
+++ src/OptionalClass/Java8Tester.java
@@ -0,0 +1,44 @@
+package OptionalClass;
+
+/**
+ * Project: TutorialsPointJava8
+ * FileName: Java8Tester
+ * Date: 2015-08-05
+ * Time: ¿ÀÀü 9:26
+ * Author: sklee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.Optional;
+
+public class Java8Tester {
+    public static void main(String args[]){
+
+        Java8Tester java8Tester = new Java8Tester();
+        Integer value1 = null;
+        Integer value2 = new Integer(10);
+
+        //Optional.ofNullable - allows passed parameter to be null.
+        Optional<Integer> a = Optional.ofNullable(value1);
+
+        //Optional.of - throws NullPointerException if passed parameter is null
+        Optional<Integer> b = Optional.of(value2);
+        System.out.println(java8Tester.sum(a,b));
+    }
+
+    public Integer sum(Optional<Integer> a, Optional<Integer> b){
+
+        //Optional.isPresent - checks the value is present or not
+
+        System.out.println("First parameter is present: " + a.isPresent());
+        System.out.println("Second parameter is present: " + b.isPresent());
+
+        //Optional.orElse - returns the value if present otherwise returns
+        //the default value passed.
+        Integer value1 = a.orElse(new Integer(0));
+
+        //Optional.get - gets the value, value should be present
+        Integer value2 = b.get();
+        return value1 + value2;
+    }
+}
